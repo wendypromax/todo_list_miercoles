cdcf6f532fb2f70a6e22d792d6941f6cecbada01
Wendy Mora <wendyv_mora@soy.sena.edu>
2025-12-10 10:50:06 -0500

    porfin

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ const mysql = require('mysql2');
 require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || 'localhost',
+  host: process.env.DB_HOST || 'caboose.proxy.rlwy.net',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'todo_db',
-  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306
+  password: process.env.DB_PASSWORD || 'NraXnkOcWALyplxrkVrQfuNPDuZwFkYX',
+  database: process.env.DB_NAME || 'railway',
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 12831
 });
 
 db.connect(err => {
